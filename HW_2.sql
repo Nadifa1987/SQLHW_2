549e81e69c0a60b29f5d002f007324389ffe28a8
Nadifa1987 <nadifa1987@mail.ru>
2022-08-23 01:41:04 +0300

    HW

---
+++ HW_2.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS Ganre (
+	Ganre_id INTEGER UNIQUE PRIMARY KEY NOT NULL,
+	Ganre_name VARCHAR(50) 
+);
+	
+CREATE TABLE IF NOT EXISTS Singer (
+	Singer_id INTEGER UNIQUE PRIMARY KEY NOT NULL,
+	Singer_name VARCHAR(80)
+);
+
+CREATE TABLE IF NOT EXISTS GanreSinger (
+	Ganre_id INTEGER REFERENCES Ganre(Ganre_id),
+	Singer_id INTEGER REFERENCES Singer(Singer_id),
+	CONSTRAINT pk PRIMARY KEY (Ganre_id, Singer_id)
+);
+	
+CREATE TABLE IF NOT EXISTS ALBUM (
+	Album_id INTEGER UNIQUE PRIMARY KEY NOT NULL,
+	Album_name VARCHAR (100),
+	Year INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS AlbumSinger (
+	Album_id INTEGER REFERENCES Album(Album_id),
+	Singer_id INTEGER REFERENCES Singer(Singer_id),
+	CONSTRAINT pk_a PRIMARY KEY (Album_id, Singer_id)
+);
+
+CREATE TABLE IF NOT EXISTS Song (
+	Song_id INTEGER UNIQUE PRIMARY KEY NOT NULL,
+	Song_name VARCHAR(100),
+	Long TIME,
+	Album_id INTEGER REFERENCES Album(Album_id)
+);
+
+CREATE TABLE IF NOT EXISTS Collection (
+	Collection_id INTEGER UNIQUE PRIMARY KEY NOT NULL,
+	Collection_name VARCHAR (100),
+	Song_id INTEGER REFERENCES Song(Song_id),
+	Year INTEGER
+);
+
+	
+	
